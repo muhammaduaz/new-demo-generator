d8a9533ba471d7f0f12c0b1a9eed3497d20d1bdc
Oliver Han <ohan88@gmail.com>
2022-09-13 11:58:59 -0700

    Checker (#4)
    * add error checking on load
    
    * add error checking on load

--- src/components/CSVReader.js
+++ src/components/CSVReader.js
@@ -1,8 +1,11 @@
 import { CSVReader } from 'react-papaparse';
 import { generateSessionId } from '../util/common.js';
+import { checkSyntax } from '../util/checker.js';
 import { toaster } from 'evergreen-ui'
+import { useState } from 'react'
 
 export default ({setEventList, setIsLoading, setCsvLoaded, analyticsSecondary}) => {
+  const [errors, setErrors] = useState([])
   const handleOnDrop = (data) => {
     setIsLoading(false)
     setCsvLoaded(true)
@@ -12,6 +15,7 @@ export default ({setEventList, setIsLoading, setCsvLoaded, analyticsSecondary}) 
       anonymousId: generateSessionId(),
       event: 'Loaded CSV',
     });
+    checkSyntax(arr, setErrors)
   };
   const handleOnError = (err, file, inputElem, reason) => {
     console.log(err);
@@ -36,7 +40,16 @@ export default ({setEventList, setIsLoading, setCsvLoaded, analyticsSecondary}) 
       >
         <span>Drop CSV file here or click to upload.</span>
       </CSVReader>
+      
       </div>
+      {errors.length > 0 
+      ? <div style={{"marginTop": "1em", "fontWeight":"bold", "color": "red", "fontSize": "16px"}}>Errors: </div>
+      : <div></div>
+      }
+      
+      {errors.map((error, index) => 
+      <div style={{"marginTop": "0.5em", "fontSize": "16px"}}key={index}>{error}</div>
+        )}
     </>
   );
   
